cc233610acfe553601cce9b199ceb7dc88280df0
Joseph Cheverton-Wynne <joseph.cheverton-wynne@bjss.com>
2021-11-13 14:08:44 +0000

    streaming parse of records

--- src/days/day04.zig
+++ src/days/day04.zig
@@ -18,13 +18,75 @@ pub fn run(contents: []u8, out: anytype) !void {
 fn solve(contents: []u8, p1: *usize, p2: *usize) void {
     var records = std.mem.split(u8, contents, "\n\n");
     while (records.next()) |recordString| {
-        var record = Record.fromString(recordString);
+        var record = Record.new();
+
+        var p2Good = true;
+
+        var lines = std.mem.split(u8, recordString, "\n");
+        while (lines.next()) |line| {
+            if (line.len == 0) continue;
+            var categories = std.mem.split(u8, line, " ");
+            while (categories.next()) |category| {
+                var cat = category[0..3];
+                var val = category[4..];
+                if (std.mem.eql(u8, cat, "byr")) {
+                    record.foundBirthYear = true;
+                    if (p2Good and !validBirthYear(val)) {
+                        p2Good = false;
+                    } else {
+                        record.validBirthYear = true;
+                    }
+                } else if (std.mem.eql(u8, cat, "iyr")) {
+                    record.foundIssueYear = true;
+                    if (p2Good and !validIssueYear(val)) {
+                        p2Good = false;
+                    } else {
+                        record.validIssueYear = true;
+                    }
+                } else if (std.mem.eql(u8, cat, "eyr")) {
+                    record.foundExpirationYear = true;
+                    if (p2Good and !validExpirationYear(val)) {
+                        p2Good = false;
+                    } else {
+                        record.validExpirationYear = true;
+                    }
+                } else if (std.mem.eql(u8, cat, "hgt")) {
+                    record.foundHeight = true;
+                    if (p2Good and !validHeight(val)) {
+                        p2Good = false;
+                    } else {
+                        record.validHeight = true;
+                    }
+                } else if (std.mem.eql(u8, cat, "hcl")) {
+                    record.foundHairColour = true;
+                    if (p2Good and !validHairColour(val)) {
+                        p2Good = false;
+                    } else {
+                        record.validHairColour = true;
+                    }
+                } else if (std.mem.eql(u8, cat, "ecl")) {
+                    record.foundEyeColour = true;
+                    if (p2Good and !validEyeColour(val)) {
+                        p2Good = false;
+                    } else {
+                        record.validEyeColour = true;
+                    }
+                } else if (std.mem.eql(u8, cat, "pid")) {
+                    record.foundPassportID = true;
+                    if (p2Good and !validPassportID(val)) {
+                        p2Good = false;
+                    } else {
+                        record.validPassportID = true;
+                    }
+                }
+            }
+        }
 
         if (record.valid1()) {
             p1.* += 1;
         }
 
-        if (record.valid2()) {
+        if (p2Good and record.valid2()) {
             p2.* += 1;
         }
     }
@@ -33,130 +95,104 @@ fn solve(contents: []u8, p1: *usize, p2: *usize) void {
 const Record = struct {
     const Self = @This();
 
-    birthYear: ?[]const u8,
-    issueYear: ?[]const u8,
-    expirationYear: ?[]const u8,
-    height: ?[]const u8,
-    hairColour: ?[]const u8,
-    eyeColour: ?[]const u8,
-    passportID: ?[]const u8,
+    foundBirthYear: bool,
+    foundIssueYear: bool,
+    foundExpirationYear: bool,
+    foundHeight: bool,
+    foundHairColour: bool,
+    foundEyeColour: bool,
+    foundPassportID: bool,
+
+    validBirthYear: bool,
+    validIssueYear: bool,
+    validExpirationYear: bool,
+    validHeight: bool,
+    validHairColour: bool,
+    validEyeColour: bool,
+    validPassportID: bool,
 
     fn new() Self {
         return Self{
-            .birthYear = null,
-            .issueYear = null,
-            .expirationYear = null,
-            .height = null,
-            .hairColour = null,
-            .eyeColour = null,
-            .passportID = null,
+            .foundBirthYear = false,
+            .foundIssueYear = false,
+            .foundExpirationYear = false,
+            .foundHeight = false,
+            .foundHairColour = false,
+            .foundEyeColour = false,
+            .foundPassportID = false,
+
+            .validBirthYear = false,
+            .validIssueYear = false,
+            .validExpirationYear = false,
+            .validHeight = false,
+            .validHairColour = false,
+            .validEyeColour = false,
+            .validPassportID = false,
         };
     }
 
-    fn fromString(source: []const u8) Self {
-        var record = Record.new();
-        var lines = std.mem.split(u8, source, "\n");
-        while (lines.next()) |line| {
-            if (line.len == 0) continue;
-            var categories = std.mem.split(u8, line, " ");
-            while (categories.next()) |category| {
-                var cat = category[0..3];
-                var val = category[4..];
-                if (std.mem.eql(u8, cat, "byr")) {
-                    record.birthYear = val;
-                } else if (std.mem.eql(u8, cat, "iyr")) {
-                    record.issueYear = val;
-                } else if (std.mem.eql(u8, cat, "eyr")) {
-                    record.expirationYear = val;
-                } else if (std.mem.eql(u8, cat, "hgt")) {
-                    record.height = val;
-                } else if (std.mem.eql(u8, cat, "hcl")) {
-                    record.hairColour = val;
-                } else if (std.mem.eql(u8, cat, "ecl")) {
-                    record.eyeColour = val;
-                } else if (std.mem.eql(u8, cat, "pid")) {
-                    record.passportID = val;
-                }
-            }
-        }
-        return record;
-    }
-
     fn valid1(self: Self) bool {
-        _ = self.birthYear orelse return false;
-        _ = self.issueYear orelse return false;
-        _ = self.expirationYear orelse return false;
-        _ = self.height orelse return false;
-        _ = self.hairColour orelse return false;
-        _ = self.eyeColour orelse return false;
-        _ = self.passportID orelse return false;
-        return true;
+        return self.foundBirthYear and self.foundIssueYear and self.foundExpirationYear and self.foundHeight and self.foundHairColour and self.foundEyeColour and self.foundPassportID;
     }
 
     fn valid2(self: Self) bool {
-        return self.validBirthYear() and self.validIssueYear() and self.validExpirationYear() and self.validHeight() and self.validHairColour() and self.validEyeColour() and self.validPassport();
+        return self.validBirthYear and self.validIssueYear and self.validExpirationYear and self.validHeight and self.validHairColour and self.validEyeColour and self.validPassportID;
     }
+};
 
-    fn validBirthYear(self: Self) bool {
-        var birthYear = std.fmt.parseInt(usize, self.birthYear orelse return false, 10) catch return false;
+fn validBirthYear(birthYearString: []const u8) bool {
+    var birthYear = std.fmt.parseInt(usize, birthYearString, 10) catch return false;
 
-        return 1920 <= birthYear and birthYear <= 2002;
-    }
+    return 1920 <= birthYear and birthYear <= 2002;
+}
 
-    fn validIssueYear(self: Self) bool {
-        var issueYear = std.fmt.parseInt(usize, self.issueYear orelse return false, 10) catch return false;
+fn validIssueYear(issueYearString: []const u8) bool {
+    var issueYear = std.fmt.parseInt(usize, issueYearString, 10) catch return false;
 
-        return 2010 <= issueYear and issueYear <= 2020;
-    }
+    return 2010 <= issueYear and issueYear <= 2020;
+}
 
-    fn validExpirationYear(self: Self) bool {
-        var expirationYear = std.fmt.parseInt(usize, self.expirationYear orelse return false, 10) catch return false;
+fn validExpirationYear(expirationYearString: []const u8) bool {
+    var expirationYear = std.fmt.parseInt(usize, expirationYearString, 10) catch return false;
 
-        return 2020 <= expirationYear and expirationYear <= 2030;
-    }
+    return 2020 <= expirationYear and expirationYear <= 2030;
+}
 
-    fn validHeight(self: Self) bool {
-        var heightString = self.height orelse return false;
-        if (heightString.len < 4) {
-            return false;
-        }
-
-        var unit = heightString[heightString.len - 2 ..];
-        var num = heightString[0 .. heightString.len - 2];
-        if (std.mem.eql(u8, unit, "cm")) {
-            var cm = std.fmt.parseInt(usize, num, 10) catch return false;
-            return 150 <= cm and cm <= 193;
-        } else if (std.mem.eql(u8, unit, "in")) {
-            var inches = std.fmt.parseInt(usize, num, 10) catch return false;
-            return 59 <= inches and inches <= 76;
-        } else {
-            return false;
-        }
-    }
-
-    fn validHairColour(self: Self) bool {
-        var hairColour = self.hairColour orelse return false;
-
-        return hairColour.len == 7 and hairColour[0] == '#' and allAlphaNumeric(hairColour[1..]);
-    }
-
-    fn validEyeColour(self: Self) bool {
-        var eyeColour = self.eyeColour orelse return false;
-        const options = [_]*const [3:0]u8{ "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
-        inline for (options) |opt| {
-            if (std.mem.eql(u8, eyeColour, opt)) {
-                return true;
-            }
-        }
+fn validHeight(height: []const u8) bool {
+    if (height.len < 4) {
         return false;
     }
 
-    fn validPassport(self: Self) bool {
-        var passportID = self.passportID orelse return false;
-
-        return passportID.len == 9 and allNumeric(passportID);
+    var unit = height[height.len - 2 ..];
+    var num = height[0 .. height.len - 2];
+    if (std.mem.eql(u8, unit, "cm")) {
+        var cm = std.fmt.parseInt(usize, num, 10) catch return false;
+        return 150 <= cm and cm <= 193;
+    } else if (std.mem.eql(u8, unit, "in")) {
+        var inches = std.fmt.parseInt(usize, num, 10) catch return false;
+        return 59 <= inches and inches <= 76;
+    } else {
+        return false;
     }
-};
+}
+
+fn validHairColour(hairColour: []const u8) bool {
+    return hairColour.len == 7 and hairColour[0] == '#' and allAlphaNumeric(hairColour[1..]);
+}
+
+fn validEyeColour(eyeColour: []const u8) bool {
+    const options = [_]*const [3:0]u8{ "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
+    inline for (options) |opt| {
+        if (std.mem.eql(u8, eyeColour, opt)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn validPassportID(passportID: []const u8) bool {
+    return passportID.len == 9 and allNumeric(passportID);
+}
 
 fn allNumeric(in: []const u8) bool {
     for (in) |i| {
